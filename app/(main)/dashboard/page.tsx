bced348826dc9a2cb6ecd6a317d926d819a7d753
Chain-Jo <chainjosephsting@gmail.com>
2024-07-25 11:07:09 -0400

    Arreglando validacion de supervisor 6

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -54,69 +54,9 @@ const DashboardPage = async () => {
     const supervisorIds = isSupervisor();
     const user = await currentUser();
     if (user != null) {
-        if (adminIds.includes(user.id) && !supervisorIds.includes(user.id)) {
-            return (
-    
-                <>
-                        <div className="flex flex-row-reverse gap-[48px] px-6">
-                        <StickyWrapper>
-                            <UserProgress 
-                                activeCourse={userProgress.activeCourse}
-                                hearts={userProgress.hearts}
-                                points={userProgress.points}
-                            />
-                            <Promo/>
-                            <Quests points={userProgress.points}/>
-                        </StickyWrapper>
-                        <FeedWrapper>
-                            <div className="w-full flex flex-col items-center">
-                                <Image
-                                    src="/img/estadisticas.png"
-                                    alt="Administrar"
-                                    height={100}
-                                    width={100}
-                                />
-                                <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">
-                                    Administrar
-                                </h1>
-                                <p className="text-muted-foreground text-center text-lg mb-6">
-                                    Accede a las funcionalidades del administrador, ve y modifica los datos de la aplicación.
-                                </p>
-        
-        
-        
-        
-        
-                                <Separator className="mb-4 h-0.5 rounded-full"/>
-        
-                                    {/**
-                                     * 
-                                     <div 
-                                    className="flex items-center w-full p-2 px-4 rounded-xl hover:bg-gray-200/50"
-                                >
-                                    * 
-                                    */}
-                                    <div 
-                                    className="flex items-center w-full p-2 px-4 rounded-xl"
-                                >
-        
-                                    <Dashboard
-                                        
-                                    />
-                                </div>
-        
-        
-        
-                            </div>
-                        </FeedWrapper>
-                    </div>
-                    
-                </>
-            );
+        if (!adminIds.includes(user.id)) {
 
-        }
-    } else {
-        return (
+            return (
 
                 <>                        ?
                         <div className="flex flex-row-reverse gap-[48px] px-6">
@@ -174,6 +114,66 @@ const DashboardPage = async () => {
                     
                 </>
             );
+        }
+    } else {
+        return (
+    
+            <>
+                    <div className="flex flex-row-reverse gap-[48px] px-6">
+                    <StickyWrapper>
+                        <UserProgress 
+                            activeCourse={userProgress.activeCourse}
+                            hearts={userProgress.hearts}
+                            points={userProgress.points}
+                        />
+                        <Promo/>
+                        <Quests points={userProgress.points}/>
+                    </StickyWrapper>
+                    <FeedWrapper>
+                        <div className="w-full flex flex-col items-center">
+                            <Image
+                                src="/img/estadisticas.png"
+                                alt="Administrar"
+                                height={100}
+                                width={100}
+                            />
+                            <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">
+                                Administrar
+                            </h1>
+                            <p className="text-muted-foreground text-center text-lg mb-6">
+                                Accede a las funcionalidades del administrador, ve y modifica los datos de la aplicación.
+                            </p>
+    
+    
+    
+    
+    
+                            <Separator className="mb-4 h-0.5 rounded-full"/>
+    
+                                {/**
+                                 * 
+                                 <div 
+                                className="flex items-center w-full p-2 px-4 rounded-xl hover:bg-gray-200/50"
+                            >
+                                * 
+                                */}
+                                <div 
+                                className="flex items-center w-full p-2 px-4 rounded-xl"
+                            >
+    
+                                <Dashboard
+                                    
+                                />
+                            </div>
+    
+    
+    
+                        </div>
+                    </FeedWrapper>
+                </div>
+                
+            </>
+        );
     }
 
 };
